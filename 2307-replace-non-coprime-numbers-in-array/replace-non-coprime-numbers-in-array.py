cb0825a4091b1c46927b726ae54f2c696b3fcd01
Shubham singh <jadounshivam89@gmail.com>
2025-09-16 22:32:19 +0530

    Time: 104 ms (95.65%) | Memory: 31.5 MB (59.78%) - LeetSync

---
+++ 2307-replace-non-coprime-numbers-in-array/replace-non-coprime-numbers-in-array.py
@@ -0,0 +1,12 @@
+class Solution:
+    from math import gcd
+    from math import lcm
+    def replaceNonCoprimes(self, nums: List[int]) -> List[int]:
+        stack = []
+        for num in nums:
+            while stack and gcd(stack[-1], num) > 1:
+                num = lcm(stack.pop(), num)
+            stack.append(num)
+        return stack
+
+        
